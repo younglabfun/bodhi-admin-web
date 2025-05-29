8fe2b55a70f5158816c0e53ca261fe8b36e6dda5
jing.yang <pagopagi@gmail.com>
2025-05-29 10:39:43 +0800

    fixed

--- src/views/user/components/my-pwd-form.vue
+++ src/views/user/components/my-pwd-form.vue
@@ -1,117 +1,149 @@
 <template>
-  <el-dialog :visible.sync="dialogVisible" :title="dialogTitle" width="30%">
-    <el-form ref="dataForm" :rules="rules" :model="params" label-position="right" label-width="100px">
+  <el-dialog
+    :visible.sync="dialogVisible"
+    :modal-append-to-body="false"
+    :title="dialogTitle"
+    width="30%"
+  >
+    <el-form
+      ref="dataForm"
+      :rules="rules"
+      :model="params"
+      label-position="right"
+      label-width="100px"
+    >
       <el-row>
         <el-col :span="22">
           <el-form-item label="原密码" prop="password">
-            <el-input type="password" v-model="params.password" placeholder="请输入密码" show-password inline-message="true" />
+            <el-input
+              type="password"
+              v-model="params.password"
+              placeholder="请输入密码"
+              show-password
+              inline-message="true"
+            />
           </el-form-item>
         </el-col>
         <el-col :span="22">
           <el-form-item label="新密码" prop="newPassword">
-            <el-input type="password" v-model="params.newPassword" placeholder="请输入密码" show-password inline-message="true" />
+            <el-input
+              type="password"
+              v-model="params.newPassword"
+              placeholder="请输入密码"
+              show-password
+              inline-message="true"
+            />
           </el-form-item>
         </el-col>
         <el-col :span="22">
           <el-form-item label="确认密码" prop="confirmPassword">
-            <el-input type="password" v-model="params.confirmPassword" placeholder="请输入确认密码" show-password inline-message="true" />
+            <el-input
+              type="password"
+              v-model="params.confirmPassword"
+              placeholder="请输入确认密码"
+              show-password
+              inline-message="true"
+            />
           </el-form-item>
         </el-col>
       </el-row>
     </el-form>
     <div slot="footer">
-      <el-button @click="cancel">
-        取消
-      </el-button>
-      <el-button type="primary" @click="updateMyPwd">
-        确定
-      </el-button>
+      <el-button @click="cancel"> 取消 </el-button>
+      <el-button type="primary" @click="updateMyPwd"> 确定 </el-button>
     </div>
   </el-dialog>
 </template>
 
 <script>
-import { validPassword, validConfirmPasswrod } from '@/utils/validate';
-import { resetPassword } from '@/api/account';
+import { validPassword, validConfirmPasswrod } from "@/utils/validate";
+import { resetPassword } from "@/api/account";
 
 export default {
-  name: 'MyPwdForm',
+  name: "MyPwdForm",
   data() {
     const validatorPassword = (rule, value, callback) => {
-      const valid = validPassword(value)
+      const valid = validPassword(value);
       if (valid != true) {
-        callback(new Error(valid))
+        callback(new Error(valid));
       } else {
-        callback()
+        callback();
       }
-    }
+    };
     const validatorConfirmPassword = (rule, value, callback) => {
-      const valid = validConfirmPasswrod(value, this.params.newPassword)
+      const valid = validConfirmPasswrod(value, this.params.newPassword);
       if (valid != true) {
-        callback(new Error(valid))
+        callback(new Error(valid));
       } else {
-        callback()
+        callback();
       }
-    }
+    };
     return {
       dialogVisible: false,
-      dialogTitle: '修改密码',
+      dialogTitle: "修改密码",
 
       params: {},
       rules: {
         password: [
-          {required: true, message: '原密码是必填项', trigger: 'blur'},
+          { required: true, message: "原密码是必填项", trigger: "blur" },
         ],
         newPassword: [
-          { required: true, trigger: 'blur', validator: validatorPassword }
+          { required: true, trigger: "blur", validator: validatorPassword },
         ],
         confirmPassword: [
-          { required: true, trigger: 'blur', validator: validatorConfirmPassword }
-        ]
-      }
-    }
+          {
+            required: true,
+            trigger: "blur",
+            validator: validatorConfirmPassword,
+          },
+        ],
+      },
+    };
   },
   methods: {
     showDialog() {
-      this.setDefault()
+      this.setDefault();
       this.$nextTick(() => {
-        this.$refs['dataForm'].clearValidate()
-      })
-      this.dialogVisible = true
+        this.$refs["dataForm"].clearValidate();
+      });
+      this.dialogVisible = true;
     },
     setDefault() {
-      this.params = Object.assign({}, {
-        password: '',
-        newPassword: '',
-        confirmPassword: '',
-      })
+      this.params = Object.assign(
+        {},
+        {
+          password: "",
+          newPassword: "",
+          confirmPassword: "",
+        }
+      );
     },
     updateMyPwd() {
-      this.$refs['dataForm'].validate((valid) => {
+      this.$refs["dataForm"].validate((valid) => {
         if (valid) {
-          var params = Object.assign({}, this.params)
+          var params = Object.assign({}, this.params);
           resetPassword(params.password, params.newPassword).then((resp) => {
-            const { message, data } = resp
+            const { message, data } = resp;
             if (data.affected) {
               this.$notify({
                 message: message,
-                type: 'success'
-              })
-              this.dialogVisible = false
-              this.$emit("resetPwd")
+                type: "success",
+              });
+              this.dialogVisible = false;
+              this.$emit("resetPwd");
             } else {
               this.$notify({
                 message: message,
-                type: 'error'
-              })
+                type: "error",
+              });
             }
-          })
+          });
         }
-      })
+      });
     },
     cancel() {
-      this.dialogVisible = false
-    }
-  }
-}
+      this.dialogVisible = false;
+    },
+  },
+};
 </script>
